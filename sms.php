b9f926e8bc933451265bfc0ac79b75b191ed830c
tonnym250 <tonnymugisha50@gmail.com>
2025-05-12 13:29:14 +0200

    sms file created

---
+++ sms.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+include_once('myutils.php');
+
+use AfricasTalking\SDK\AfricasTalking;
+
+class Sms {
+    protected $phone;
+    protected $AT;
+
+    public function __construct($phone) {
+        $this->phone = $phone;
+
+        $this->AT = new AfricasTalking(
+            myutils::$API_USERNAME,
+            myutils::$API_KEY
+        );
+    }
+
+    public function sendSMS($message, $recipients) {
+        $sms = $this->AT->sms();
+
+        $result = $sms->send([
+            'username' => myutils::$API_USERNAME,
+            'to'       => $recipients,
+            'message'  => $message,
+            'from'     => myutils::$COMPANY_NAME 
+        ]);
+
+        return $result;
+    }
+}
+?>
